3555ccf4b6ac361eda6a293ee3027c7d0cc7144e
jyotiprakash-m <teachmeonline365@gmail.com>
2023-06-08 14:31:24 +0530

    enable cors

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ import connect from "./utils/db.js";
 import Post from "./models/Post.js";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import cors from "cors";
 dotenv.config();
 const app = express();
 const port = process.env.PORT | 3000;
 app.use(bodyParser.json());
+app.use(cors());
 // Create post
 
 app.post("/", async (req, res) => {
